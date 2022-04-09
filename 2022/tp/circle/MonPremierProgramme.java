f657e139993684ecd0e47081a517ccefbe697d08
moctar.compaore <moctar.compaore@gmail.com>
2022-04-09 16:28:19 +0000

    le rayon C de Circle  peut être passé en paramétres

--- 2022/tp/circle/MonPremierProgramme.java
+++ 2022/tp/circle/MonPremierProgramme.java
@@ -12,7 +12,7 @@ class Circle {
 public class MonPremierProgramme {
 	public static void main(String[] args) {
 		Circle c; // c est une référence sur un objet de type Circle, pas encore un objet
-		c = new Circle(5.0); // c référence maintenant un objet alloué en mémoire
+		c = new Circle(Double.parseDouble(args[0])); // c référence maintenant un objet alloué en mémoire
 		c.x = c.y = 10;  // ces valeurs sont stockées dans le corps de l’objet
 		System.out.println("Aire de c :" + c.area());
 	}
