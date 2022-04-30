fe68f54f814591342451fcd62ac80a692dddc638
moctar.compaore <moctar.compaore@gmail.com>
2022-04-30 18:51:28 +0000

    ajout du fichier module.java dans le  projet programme

---
+++ 2022/tp/Programme/app/src/main/java/bf/ist/rmi/programme/Module.java
@@ -0,0 +1,33 @@
+package bf.ist.rmi.programme;
+
+import java.io.Serializable;
+
+public class Module implements Serializable {
+    private  String libelle;
+    private String description;
+    private Integer duree;
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDuree() {
+        return duree;
+    }
+
+    public void setDuree(Integer duree) {
+        this.duree = duree;
+    }
+}
